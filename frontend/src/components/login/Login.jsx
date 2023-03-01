24b5cf599b35682c706d2a14e9188dafeadfca00
gmarte <giancarlo.marte@gmail.com>
2023-03-01 01:21:00 -0400

    frontend navbar and general style

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -1,60 +1,15 @@
 import React from 'react';
 import './login.css';
-import {
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-  MDBBtn,
-  MDBIcon,
-  MDBInput,
-  MDBCheckbox
-}
-from 'mdb-react-ui-kit';
+import mascot from '../../assets/FTM_Mascot_New.png'
 
 const Login = () => {
   return (
-    <MDBContainer fluid className="p-3 my-5">
-
-      <MDBRow>
-
-        <MDBCol col='10' md='6'>
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image" />
-        </MDBCol>
-
-        <MDBCol col='4' md='6'>
-
-
-          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg"/>
-          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg"/>
-
-
-          <div className="d-flex justify-content-between mx-4 mb-4">
-            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-            <a href="!#">Forgot password?</a>
-          </div>
-
-          <MDBBtn className="mb-4 w-100" size="lg">Sign in</MDBBtn>
-
-          <div className="divider d-flex align-items-center my-4">
-            <p className="text-center fw-bold mx-3 mb-0">OR</p>
-          </div>
-
-          <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#3b5998'}}>
-            <MDBIcon fab icon="facebook-f" className="mx-2"/>
-            Continue with facebook
-          </MDBBtn>
-
-          <MDBBtn className="mb-4 w-100" size="lg" style={{backgroundColor: '#55acee'}}>
-            <MDBIcon fab icon="twitter" className="mx-2"/>
-            Continue with twitter
-          </MDBBtn>
-
-        </MDBCol>
-
-      </MDBRow>
-
-    </MDBContainer>
-  );
+    <div className='ftm__login'>
+      <div className='ftm__login-hero_image'>
+        <img src={mascot} />
+      </div>      
+    </div>
+  )
 }
 
-export default Login;
+export default Login
--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,20 +1,49 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLin, RiCloseLine} from 'react-icons/ri';
 import './navbar.css';
-import logo from '../../assets/logo.svg';
+import logo from '../../assets/FTM_LOGO.png';
+
+const Menu = () => (
+  <>
+  <p><a href='#home'>Home</a></p>
+  <p><a href='#chores'>Chores</a></p>
+  <p><a href='#rewards'>Rewards</a></p>
+  <p><a href='#children'>Childen</a></p>
+  <p><a href='#badges'>Badges</a></p>
+  </>
+)
 const Navbar = () => {
+  const [toogleMenu, setToogleMenu] = useState(false);
   return (
-    <div className='gpt3__navbar'>
-      <div className='gpt3__navbar-links'>
-        <div className='gpt3__navbar-links_logo'>
-          <img src={logo} alt="logo" />
+    <div className='ftm__navbar'>
+      <div className='ftm__navbar-links'>        
+        <div className='ftm__navbar-links_logo'>          
+        <img src={logo} alt="logo" />
+        </div>        
+        <div className='ftm__navbar-links_container'>
+          <Menu />
         </div>
-        <div className='gpt3__navbar-links_container'>
-          <p><a href='#home'>Home</a></p>
-          <p><a href='#child'>Child</a></p>
-          <p><a href='#tasks'>Tasks</a></p>
-          <p><a href='#rewards'>Rewards</a></p>
-        </div>
+      </div>
+      <div className='ftm__navbar-sign'>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className='ftm__navbar-menu'>
+        {toogleMenu
+        ? <RiCloseLine color='#fff' size={27} onClick={() => setToogleMenu(false)} />
+        : <RiMenu3Line color='#fff' size={27} onClick={() => setToogleMenu(true)} />
+        }
+        {toogleMenu && (
+          <div className='ftm__navbar-menu_container scale-up-center'>
+            <div className='ftm__navbar-menu_container-links'>
+              <Menu />
+              <div className='ftm__navbar-menu_container-links-sign'>
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
