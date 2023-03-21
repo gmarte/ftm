b1afae10081fb64cb1613239fbde677ad5089ead
gmarte <giancarlo.marte@gmail.com>
2023-03-20 23:36:43 -0400

    persistant login attempt

--- ftm/settings.py
+++ ftm/settings.py
@@ -109,6 +109,9 @@ CORS_ORIGIN_WHITELIST = (
 )
 
 REST_FRAMEWORK = {
+    # "DEFAULT_PERMISSION_CLASSES": [
+    # "rest_framework.permissions.IsAuthenticated",
+    # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (        
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
